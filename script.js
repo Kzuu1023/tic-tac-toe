e34266a447ae6c4b534e3181513926aa6479a096
kzuu1023 <161953460+Kzuu1023@users.noreply.github.com>
2024-09-18 11:05:50 -0700

    refactor: alternative X and O

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tiles = document.querySelectorAll(".tiles");
-
+let spaces = Array(9).fill(null);
+let currentPlayer = "";
 function startGame() {
     let ask = prompt("Wanna play a game?");
     if (ask.toUpperCase() === "Y") {
@@ -8,33 +9,44 @@ function startGame() {
 }
 
 function gameBoard() {
-    let iconElem = "";
     let askPlayer = prompt("X or O").toUpperCase();
-    let playerX, playerO;
+    let player1, player2;
+
+    let iconElem;
 
     if (askPlayer !== "X" && askPlayer !== "O") {
         alert("Invalid choice. Please choose X or O.");
         return;
     }
 
+    currentPlayer = player1;
+
     tiles.forEach((tilesBtn) => {
         tilesBtn.addEventListener("click", function () {
             switch (askPlayer) {
                 case "X":
-                    playerX = "./assets/icon-x.svg";
+                    player1 = "./assets/icon-x.svg";
+                    player2 = "./assets/icon-o.svg";
                     iconElem = document.createElement("img");
-                    iconElem.src = playerX;
+                    iconElem.src = currentPlayer;
                     tilesBtn.appendChild(iconElem);
                     break;
                 case "O":
-                    playerO = "./assets/icon-o.svg";
+                    player1 = "./assets/icon-o.svg";
+                    player2 = "./assets/icon-x.svg";
                     iconElem = document.createElement("img");
-                    iconElem.src = playerO;
+                    iconElem.src = currentPlayer;
                     tilesBtn.appendChild(iconElem);
                     break;
+
                 default:
-                    playerX, (playerO = "");
-                    break;
+                    return;
+            }
+
+            if (currentPlayer == player1) {
+                currentPlayer = player2;
+            } else {
+                currentPlayer = player1;
             }
         });
     });
