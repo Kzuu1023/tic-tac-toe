072e92e997ae8a922cc023c66914c7b0371d0419
kzuu1023 <161953460+Kzuu1023@users.noreply.github.com>
2024-09-21 10:12:42 -0700

    feat: fix duplicate value per tiles

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let tiles = document.querySelectorAll(".tiles");
+const tiles = document.querySelectorAll(".tiles");
+const playerScore = document.querySelectorAll(".msg");
 let spaces = Array(9).fill(null);
 let currentPlayer = "";
+let score = "";
+
+const winCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 function startGame() {
     let ask = prompt("Wanna play a game?");
     if (ask.toUpperCase() === "Y") {
@@ -12,44 +26,50 @@ function gameBoard() {
     let askPlayer = prompt("X or O").toUpperCase();
     let player1, player2;
 
-    let iconElem;
-
     if (askPlayer !== "X" && askPlayer !== "O") {
         alert("Invalid choice. Please choose X or O.");
         return;
     }
 
+    switch (askPlayer) {
+        case "X":
+            player1 = "./assets/icon-x.svg";
+            player2 = "./assets/icon-o.svg";
+            break;
+        case "O":
+            player1 = "./assets/icon-o.svg";
+            player2 = "./assets/icon-x.svg";
+            break;
+        default:
+            return;
+    }
+
     currentPlayer = player1;
 
-    tiles.forEach((tilesBtn) => {
+    tiles.forEach((tilesBtn, index) => {
         tilesBtn.addEventListener("click", function () {
-            switch (askPlayer) {
-                case "X":
-                    player1 = "./assets/icon-x.svg";
-                    player2 = "./assets/icon-o.svg";
-                    iconElem = document.createElement("img");
-                    iconElem.src = currentPlayer;
-                    tilesBtn.appendChild(iconElem);
-                    break;
-                case "O":
-                    player1 = "./assets/icon-o.svg";
-                    player2 = "./assets/icon-x.svg";
-                    iconElem = document.createElement("img");
-                    iconElem.src = currentPlayer;
-                    tilesBtn.appendChild(iconElem);
-                    break;
-
-                default:
-                    return;
+            if (!spaces[index]) {
+                spaces[index] = currentPlayer;
+                let iconElem = document.createElement("img");
+                iconElem.src = currentPlayer;
+                tilesBtn.appendChild(iconElem);
             }
 
-            if (currentPlayer == player1) {
+            //alternative value X and O
+            if (currentPlayer === player1) {
                 currentPlayer = player2;
             } else {
                 currentPlayer = player1;
             }
         });
     });
+}
+
+function determineWinner() {
+    if (currentPlayer === winCombination) {
+        score++;
+        playerScore.textContent = score;
+    }
 }
 
 startGame();
